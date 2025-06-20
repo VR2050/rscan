933f3a9ef24f4358b4e3dfe8e839589fcceb6e79
VR2050 <2779753429@qq.com>
2025-06-20 14:29:51 +0800

    test

--- src/services/poc_scan.rs
+++ src/services/poc_scan.rs
@@ -1,6 +1,6 @@
 // POCSCAN 结构体用于漏洞检测，携带 HTTP 头部、匹配函数和匹配值
 use reqwest::{Response, header::HeaderMap};
-pub struct POCSCAN<F, T>
+pub struct POCSCAN<F, T>  
 where
     F: Fn(&Response, T) -> bool,
 {
